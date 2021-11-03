d68f15cad34ac2726dfec0154bfb98873d87cf71
ryohei-miki <miki@n2i.jp>
2021-11-03 21:40:09 +0900

    first commit

---
+++ db/migrate/20211103110446_create_tests.rb
@@ -0,0 +1,10 @@
+class CreateTests < ActiveRecord::Migration[6.1]
+  def change
+    create_table :tests do |t|
+      t.string :uuid
+      t.string :name
+
+      t.timestamps
+    end
+  end
+end
